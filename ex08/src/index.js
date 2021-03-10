439266cbbc44d38883ca696615024e584d987669
DenisBukva <denisbukva@hotmail.com>
2021-03-10 15:53:37 +0100

    add ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,23 @@
+function main(numOne, numTwoo) {
+sum = numOne + numTwoo
+
+var sum;
+var numOne;
+var numTwoo;
+
+
+
+
+
+return sum;
+
+} 
+
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
+
+
+
