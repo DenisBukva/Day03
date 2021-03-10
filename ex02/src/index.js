60b35c9d5e2ccfa17b8bfff1fc12545f4d8c60da
DenisBukva <denisbukva@hotmail.com>
2021-03-10 13:20:05 +0100

    add ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    console.log ('Hello World!');
+    /*document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");*/
+    // This is a single line JavaScript comment
+    // This is a single line JavaScript comment
+    // This is a single line JavaScript comment
+    // This is a single line JavaScript comment
+    // This is a single line JavaScript comment
+    
+
+    
+}
+main();
+module.exports=main;
