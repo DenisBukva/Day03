8382eec6f5d21c2844fdddcaf12b1aeeacbdeefc
DenisBukva <denisbukva@hotmail.com>
2021-03-10 16:32:29 +0100

    add ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwoo) {
+product = numOne * numTwoo
+
+var product;
+var numOne;
+var numTwoo;
+
+
+
+
+
+    return product;
+}
+
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+
+module.exports = main;
