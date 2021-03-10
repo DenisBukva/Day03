dbdb2f696a12bdeb79b926a2e6462e0ea82359a3
DenisBukva <denisbukva@hotmail.com>
2021-03-10 16:21:44 +0100

    add ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwoo) {
+difference = numOne - numTwoo
+
+var difference;
+var numOne;
+var numTwoo;
+
+
+
+
+
+    return difference;
+}
+
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
+
+
+module.exports = main;
